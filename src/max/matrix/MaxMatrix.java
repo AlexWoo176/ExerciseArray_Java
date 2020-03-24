30ec77e815ec9169f3dfa96114bb00a7cacdf4fc
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-03-24 10:26:12 +0700

    fixes

--- src/max/matrix/MaxMatrix.java
+++ src/max/matrix/MaxMatrix.java
@@ -13,32 +13,47 @@ public class MaxMatrix {
         double[][] matrix = new double[row][col];
         for (int i = 0; i < matrix.length ; i++) {
             for (int j = 0; j < matrix[i].length ; j++) {
-                System.out.print("Enter Element: ");
-                matrix[i][j] = scanner.nextDouble();
+                matrix[i][j] = (int) Math.floor(Math.random()*1000);
+//                System.out.print("Enter Element: ");
+//                matrix[i][j] = scanner.nextDouble();
             }
         }
 
-        System.out.println("Property list: ");
+        System.out.println("The Element Array List: ");
         for (int i = 0; i < matrix.length ; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] +" ");
+                System.out.printf("%-7.1f", matrix[i][j]);
+//                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
 
         double max = matrix[0][0];
-        int indexRow = 1;
-        int indexCol = 1;
+//        int indexRow = 1;
+//        int indexCol = 1;
 
         for (int i = 0; i < matrix.length ; i++) {
             for (int j = 0; j < matrix[i].length ; j++) {
                 if (max < matrix[i][j]) {
                     max = matrix[i][j];
-                    indexRow = i + 1;
-                    indexCol = j + 1;
+//                    indexRow = i + 1;
+//                    indexCol = j + 1;
                 }
             }
         }
-        System.out.println("The largest property value in the list is " + max + " ,at position row " + indexRow +" ,at position column " + indexCol);
+
+        String maxPosition = "";
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = 0; j < matrix[i].length ; j++) {
+                if (max == matrix[i][j]) {
+                    max = matrix[i][j];
+                    maxPosition +="["+ i +"]["+ j +"]";
+                }
+            }
+        }
+
+        System.out.printf("The largest property value in the list is : %.1f at position %s", max, maxPosition);
+
+//        System.out.println("The largest property value in the list is " + max + " ,at position row " + indexRow +" ,at position column " + indexCol);
     }
 }
